af40c3129e7b3fceb7e3f3fd8219fecedfcca7db
harinbeee <luv_lyn@naver.com>
2025-03-14 20:20:22 +0900

    surin 3

--- src/main/java/com/example/demo/hyungchul/PocketMon.java
+++ src/main/java/com/example/demo/hyungchul/PocketMon.java
@@ -3,5 +3,6 @@ package com.example.demo.hyungchul;
 public class PocketMon {
     public static void main(String[] args) {
         int i =0;
+        System.out.println("PocketMon.main");
     }
 }
